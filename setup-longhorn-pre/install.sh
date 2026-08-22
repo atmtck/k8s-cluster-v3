4980055b280d33f51cb691854b00cc1efd1e6da2
atmtc <riccardo@tuta.io>
2026-08-22 12:48:53 +0200

    aggiunta installazione longhornctl ai prerequisiti

--- setup-longhorn-pre/install.sh
+++ setup-longhorn-pre/install.sh
@@ -7,3 +7,7 @@ nfs
 iscsi_tcp
 dm_crypt
 EOF
+
+curl -LO "https://github.com/longhorn/cli/releases/download/v1.12.1/longhornctl-linux-amd64"
+mv longhornctl-linux-amd64 /usr/local/bin/longhornctl
+chmod 700 /usr/local/bin/longhornctl
